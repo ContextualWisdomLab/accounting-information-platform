4097a6d0006ff22421d2ac7cbd11beb560bc2807
Seongho Bae <me@seonghobae.me>
2026-09-08 14:07:53 +0900

    test(reconciliation): bind retention upgrade role to seeded tenant

--- tests/test_reconciliation_outbox_retention_preflight_postgres.py
+++ tests/test_reconciliation_outbox_retention_preflight_postgres.py
@@ -15,6 +15,9 @@ from tests import test_postgres_posting as posting
 from tests.test_reconciliation_exception_resolution_postgres import (
     ReconciliationExceptionResolutionPostgresTests,
 )
+from tests.test_reconciliation_recording_time_upgrade_postgres import (
+    ReconciliationRecordingTimeUpgradePostgresTests,
+)
 
 
 class ReconciliationOutboxRetentionPreflightPostgresTests(unittest.TestCase):
@@ -103,135 +106,140 @@ class ReconciliationOutboxRetentionPreflightPostgresTests(unittest.TestCase):
 
             self._apply_pre_retention_chain(migration_url)
 
-            with mock.patch.object(posting, "DATABASE_URL", migration_url):
+            with mock.patch.object(posting, "DATABASE_URL", admin_url):
                 fixture = ReconciliationExceptionResolutionPostgresTests(
                     "test_named_command_resolves_exception_and_emits_atomic_outbox"
                 )
                 fixture.setUp()
-                resolution = resolve_reconciliation_exception(
-                    fixture._command(),
-                    migration_url,
-                    fixture.fixture.case.policy.tenant_reference,
+            ReconciliationRecordingTimeUpgradePostgresTests._bind_role_to_tenant(
+                admin_url,
+                role_name,
+                fixture.fixture.case.tenant_id,
+            )
+            resolution = resolve_reconciliation_exception(
+                fixture._command(),
+                migration_url,
+                fixture.fixture.case.policy.tenant_reference,
+            )
+
+            with psycopg.connect(admin_url, autocommit=True) as admin_database:
+                authority = admin_database.execute(
+                    """
+                    SELECT tenant_account_id,
+                           reconciliation_exception_id,
+                           reconciliation_exception_resolution_command_id,
+                           target_resolution_status_code,
+                           reconciliation_exception_resolution_command_hash
+                    FROM accounting_core.reconciliation_exception_resolution_command
+                    WHERE reconciliation_exception_resolution_command_id = %s
+                    """,
+                    (resolution["reconciliation_exception_resolution_id"],),
+                ).fetchone()
+                assert authority is not None
+                event_type = (
+                    "reconciliation_exception_resolved"
+                    if authority[3] == "resolved"
+                    else "reconciliation_exception_superseded"
                 )
+                aggregate_reference = (
+                    "urn:cwl:accounting:reconciliation_exception:" + str(authority[1])
+                )
+                payload_reference = (
+                    "urn:cwl:accounting:reconciliation_exception_resolution:"
+                    + str(authority[2])
+                )
+                deleted = admin_database.execute(
+                    """
+                    DELETE FROM accounting_integration.outbox_event
+                    WHERE tenant_account_id = %s
+                      AND event_type_code = %s
+                      AND aggregate_reference = %s
+                      AND payload_reference = %s
+                      AND payload_hash = %s
+                    """,
+                    (
+                        authority[0],
+                        event_type,
+                        aggregate_reference,
+                        payload_reference,
+                        authority[4],
+                    ),
+                ).rowcount
+                self.assertEqual(deleted, 1)
 
-                with psycopg.connect(admin_url, autocommit=True) as admin_database:
-                    authority = admin_database.execute(
-                        """
-                        SELECT tenant_account_id,
-                               reconciliation_exception_id,
-                               reconciliation_exception_resolution_command_id,
-                               target_resolution_status_code,
-                               reconciliation_exception_resolution_command_hash
-                        FROM accounting_core.reconciliation_exception_resolution_command
-                        WHERE reconciliation_exception_resolution_command_id = %s
-                        """,
-                        (resolution["reconciliation_exception_resolution_id"],),
-                    ).fetchone()
-                    assert authority is not None
-                    event_type = (
-                        "reconciliation_exception_resolved"
-                        if authority[3] == "resolved"
-                        else "reconciliation_exception_superseded"
-                    )
-                    aggregate_reference = (
-                        "urn:cwl:accounting:reconciliation_exception:" + str(authority[1])
-                    )
-                    payload_reference = (
-                        "urn:cwl:accounting:reconciliation_exception_resolution:"
-                        + str(authority[2])
-                    )
-                    deleted = admin_database.execute(
-                        """
-                        DELETE FROM accounting_integration.outbox_event
-                        WHERE tenant_account_id = %s
-                          AND event_type_code = %s
-                          AND aggregate_reference = %s
-                          AND payload_reference = %s
-                          AND payload_hash = %s
-                        """,
-                        (
-                            authority[0],
-                            event_type,
-                            aggregate_reference,
-                            payload_reference,
-                            authority[4],
-                        ),
-                    ).rowcount
-                    self.assertEqual(deleted, 1)
-
-                with self.assertRaisesRegex(
-                    psycopg.Error,
-                    "reconciliation_authority_outbox_retention_preflight",
-                ):
-                    with psycopg.connect(
-                        migration_url,
-                        autocommit=True,
-                        cursor_factory=psycopg.ClientCursor,
-                    ) as migration_connection:
-                        migration_connection.execute(migration_0024)
-
-                with psycopg.connect(admin_url, autocommit=True) as admin_database:
-                    failed_upgrade_policies = admin_database.execute(
-                        """
-                        SELECT policyname
-                        FROM pg_catalog.pg_policies
-                        WHERE policyname LIKE
-                              'reconciliation_authority_retention_upgrade_%_visibility'
-                        ORDER BY policyname
-                        """
-                    ).fetchall()
-                    self.assertEqual(failed_upgrade_policies, [])
-                    admin_database.execute(
-                        """
-                        INSERT INTO accounting_integration.outbox_event (
-                            tenant_account_id,
-                            event_type_code,
-                            aggregate_reference,
-                            payload_reference,
-                            payload_hash
-                        )
-                        VALUES (%s, %s, %s, %s, %s)
-                        """,
-                        (
-                            authority[0],
-                            event_type,
-                            aggregate_reference,
-                            payload_reference,
-                            authority[4],
-                        ),
-                    )
-
+            with self.assertRaisesRegex(
+                psycopg.Error,
+                "reconciliation_authority_outbox_retention_preflight",
+            ):
                 with psycopg.connect(
                     migration_url,
                     autocommit=True,
                     cursor_factory=psycopg.ClientCursor,
                 ) as migration_connection:
                     migration_connection.execute(migration_0024)
-                    role_row = migration_connection.execute(
-                        "SELECT rolbypassrls FROM pg_catalog.pg_roles "
-                        "WHERE rolname = current_user"
-                    ).fetchone()
-                    remaining_policies = migration_connection.execute(
-                        """
-                        SELECT policyname
-                        FROM pg_catalog.pg_policies
-                        WHERE policyname LIKE
-                              'reconciliation_authority_retention_upgrade_%_visibility'
-                        ORDER BY policyname
-                        """
-                    ).fetchall()
-                    durable_trigger = migration_connection.execute(
-                        """
-                        SELECT count(*)
-                        FROM pg_catalog.pg_trigger
-                        WHERE tgname = 'reconciliation_authority_outbox_retention_delete_guard'
-                          AND NOT tgisinternal
-                        """
-                    ).fetchone()
 
-                self.assertEqual(role_row, (False,))
-                self.assertEqual(remaining_policies, [])
-                self.assertEqual(durable_trigger, (1,))
+            with psycopg.connect(admin_url, autocommit=True) as admin_database:
+                failed_upgrade_policies = admin_database.execute(
+                    """
+                    SELECT policyname
+                    FROM pg_catalog.pg_policies
+                    WHERE policyname LIKE
+                          'reconciliation_authority_retention_upgrade_%_visibility'
+                    ORDER BY policyname
+                    """
+                ).fetchall()
+                self.assertEqual(failed_upgrade_policies, [])
+                admin_database.execute(
+                    """
+                    INSERT INTO accounting_integration.outbox_event (
+                        tenant_account_id,
+                        event_type_code,
+                        aggregate_reference,
+                        payload_reference,
+                        payload_hash
+                    )
+                    VALUES (%s, %s, %s, %s, %s)
+                    """,
+                    (
+                        authority[0],
+                        event_type,
+                        aggregate_reference,
+                        payload_reference,
+                        authority[4],
+                    ),
+                )
+
+            with psycopg.connect(
+                migration_url,
+                autocommit=True,
+                cursor_factory=psycopg.ClientCursor,
+            ) as migration_connection:
+                migration_connection.execute(migration_0024)
+                role_row = migration_connection.execute(
+                    "SELECT rolbypassrls FROM pg_catalog.pg_roles "
+                    "WHERE rolname = current_user"
+                ).fetchone()
+                remaining_policies = migration_connection.execute(
+                    """
+                    SELECT policyname
+                    FROM pg_catalog.pg_policies
+                    WHERE policyname LIKE
+                          'reconciliation_authority_retention_upgrade_%_visibility'
+                    ORDER BY policyname
+                    """
+                ).fetchall()
+                durable_trigger = migration_connection.execute(
+                    """
+                    SELECT count(*)
+                    FROM pg_catalog.pg_trigger
+                    WHERE tgname = 'reconciliation_authority_outbox_retention_delete_guard'
+                      AND NOT tgisinternal
+                    """
+                ).fetchone()
+
+            self.assertEqual(role_row, (False,))
+            self.assertEqual(remaining_policies, [])
+            self.assertEqual(durable_trigger, (1,))
         finally:
             if fixture is not None:
                 fixture.doCleanups()
